a5d58326c846651a495f2d636e619d30d6c13a67
Fredrik Sundstrom <fsund001@gold.ac.uk>
2018-08-07 09:32:32 +0100

    First commit

---
+++ CopyingDirectory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CopyingLibrary;
+
+namespace CopyingDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source=null;
+            string target=null;
+
+            Class1 test = new Class1();
+
+            Console.WriteLine("Welcome to CopyingDirectory");
+            Console.WriteLine("Input source directory path: ");
+            source = Console.ReadLine();
+            //source = "C:\\Users\\Vandalay\\Documents\\testFolder";
+            Console.WriteLine("Input target directory path: ");
+            target = Console.ReadLine();
+            //target = "C:\\Users\\Vandalay\\Documents\\testOutput";
+           
+            Console.WriteLine("Source path is {0}: " , source);
+            Console.WriteLine("Target path is {0}: ", target);
+
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target))
+            {
+                Console.WriteLine("Source and target paths can't be empty!");
+            }
+            else
+            {
+                test.CopyingMethod(source, target);
+            }
+                    
+           
+            Console.WriteLine("Press any key to exit program");
+            Console.ReadKey();
+
+        }
+    }
+}
